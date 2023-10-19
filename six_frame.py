d10bf45a9715898158faa623fab93e6b33fb7afb
Revkxd <79917173+Revkxd@users.noreply.github.com>
2023-10-19 23:01:54 +0800

    Update six_frame.py

--- six_frame.py
+++ six_frame.py
@@ -25,13 +25,23 @@ def translate_codon(codon):
 def get_score(scoring, dna, protein, i, j):
     return scoring[translate_codon(dna[i-1:i+2])][protein[j-1]] if translate_codon(dna[i-1:i+2]) != 'FAIL' else -999
 
-def three_frame(dna_input, protein_input):
+def matrix_printer(matrices: list[list]):
+    for matrix in matrices:
+        print(matrix)
+        for row in matrix:
+            print(row)
+        print()
+
+def modded_three_frame(dna_input, protein_input):
     N, M = len(dna_input), len(protein_input)
     gep, gop = 2, 3
     frameshift_penalty = 4
     I = [[0 for col in range(M+1)] for row in range(N)]
     D = [[0 for col in range(M+1)] for row in range(N)]
     C = [[0 for col in range(M+1)] for row in range(N)]
+    TI = [[0 for col in range(M+1)] for row in range(N)]
+    TD = [[0 for col in range(M+1)] for row in range(N)]
+    TC = [[0 for col in range(M+1)] for row in range(N)]
     scoring = bl.BLOSUM(62, default=0)
 
     # Initialization
@@ -39,9 +49,13 @@ def three_frame(dna_input, protein_input):
         I[j][0] = float('-inf')
         D[0][j] = D[2][j] = D[3][j] = float('-inf')
         D[1][j] = C[0][j] - gop - gep
+        TI[j][0] = float('-inf')
+        TD[0][j] = TD[2][j] = TD[3][j] = float('-inf')
+        TD[1][j] = 1
 
     # Note: Placed j-1 for accessing protein_input since index out of bounds error
     C[0][0] = 0
+    TC[0][0] = 0
     for j in range(1, M+1):
         C[0][j] = 0
         C[j][0] = 0
@@ -57,6 +71,16 @@ def three_frame(dna_input, protein_input):
                       C[1][j-1] + get_score(scoring, dna_input, protein_input, 4, j),
                       C[2][j-1] + get_score(scoring, dna_input, protein_input, 4, j) - frameshift_penalty)
 
+        TC[0][j] = 0
+        TC[j][0] = 0
+        TC[1][j] = -2 if C[1][j] == I[1][j] else -1 if C[1][j] == D[1][j] else 1 if C[1][j] == C[0][j-1] + get_score(scoring, dna_input, protein_input, 1, j) else 0
+        TC[2][j] = -2 if C[2][j] == I[2][j] else 2 if C[2][j] == C[0][j-1] + get_score(scoring, dna_input, protein_input, 2, j) - frameshift_penalty else 0
+        TC[3][j] = -2 if C[3][j] == I[3][j] else 2 if C[3][j] == C[1][j-1] + get_score(scoring, dna_input, protein_input, 3, j) - frameshift_penalty else 0
+        TC[4][j] = -2 if C[4][j] == I[4][j] else -1 if C[4][j] == D[4][j] else 3 if C[4][j] == C[1][j-1] + get_score(scoring, dna_input, protein_input, 4, j) else 2 if C[4][j] == C[2][j-1] + get_score(scoring, dna_input, protein_input, 4, j) - frameshift_penalty else 0
+
+    # Turn all negative values in C to 0
+    C = [[0 if x < 0 else x for x in row] for row in C]
+
     # Matrix filling
     for i in range(N):
         for j in range(1, M+1):
@@ -68,14 +92,20 @@ def three_frame(dna_input, protein_input):
                           C[i-2][j-1] + get_score(scoring, dna_input, protein_input, i, j) - frameshift_penalty,
                           C[i-3][j-1] + get_score(scoring, dna_input, protein_input, i, j),
                           C[i-4][j-1] + get_score(scoring, dna_input, protein_input, i, j) - frameshift_penalty)
-            if i == N-1 and j == M:
-                # Note: -1 in index is to account for 0-indexing
-                C[N-1][M] = max(C[N-1-1][M],
-                                C[N-2-1][M] - frameshift_penalty,
-                                C[N-3-1][M] - gop - gep - frameshift_penalty,
-                                C[N-4-1][M] - frameshift_penalty - gep)
 
-    return C, C[N-1][M]
+    # Traceback matrix filling
+    for i in range(N):
+        for j in range(M):
+            TI[i][j] = 0 if I[i][j] == I[i][j-1] - gep else 1 if I[i][j] == C[i][j-1] - gop - gep else float('-inf')
+            TD[i][j] = 0 if D[i][j] == D[i-3][j] - gep else 1 if D[i][j] == C[i-3][j] - gop - gep else float('-inf')
+            if C[i][j] == I[i][j]: TC[i][j] = -2
+            elif C[i][j] == D[i][j]: TC[i][j] = -1
+            elif C[i][j] == C[i-2][j-1] + get_score(scoring, dna_input, protein_input, i, j) - frameshift_penalty: TC[i][j] = 2
+            elif C[i][j] == C[i-3][j-1] + get_score(scoring, dna_input, protein_input, i, j): TC[i][j] = 3
+            elif C[i][j] == C[i-4][j-1] + get_score(scoring, dna_input, protein_input, i, j) - frameshift_penalty: TC[i][j] = 4
+
+    matrix_printer([I, D, C])
+    return C
 
 def reverse(seq):
     """Returns a reversed string"""
@@ -104,8 +134,10 @@ def reverse_complement(seq):
     return seq
 
 def six_frame(dna_input, protein_input):
-    three_frame(dna_input, protein_input)
-    three_frame(reverse_complement(dna_input), protein_input)
+    C1 = modded_three_frame(dna_input, protein_input)
+    C2 = modded_three_frame(reverse_complement(dna_input), protein_input)
+
+    return max(max(max(C1)), max(max(C2)))
 
 if __name__ == '__main__':
     dna_inputs = ['ATGCGA', 'ATGCGATACGCTTGA', 'CTTGGTCCGAAT']
@@ -114,7 +146,6 @@ if __name__ == '__main__':
     # print(thing)
 
     for dna_input, protein_input in zip(dna_inputs, protein_inputs):
-        matr, ans = six_frame(dna_input, protein_input)
-        for row in matr:
-            print(row)
+        ans = six_frame(dna_input, protein_input)
         print(f'Score: {ans}\n')
+        break
