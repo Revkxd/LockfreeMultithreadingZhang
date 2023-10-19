0b256e446210599ff4a1acc51a19a4f1903db641
Revkxd <79917173+Revkxd@users.noreply.github.com>
2023-10-19 22:58:00 +0800

    Python match statement

--- test.py
+++ test.py
@@ -5,3 +5,10 @@ zz = 20
 
 ans = 0
 ans = 2 if val == 2 else 1 if thing == 1 else 20 if zz == 20 else 0
+
+# Python match statement (similar usage to switch case)
+value = 2.0
+match value:
+    case 1.0: print("value is 1.0")
+    case 2.0: print("value is 2.0")
+    case _: print("value is neither 1.0 nor 2.0")
